446b9da1fd678651a9b2a61cabec04210296649e
McAronDev <romanadt@gmail.com>
2017-06-10 13:42:07 +0300

    Create angular-bootstrap-toggle.js

--- src/angular-bootstrap-toggle.js
+++ src/angular-bootstrap-toggle.js
@@ -88,18 +88,18 @@
             var wrapperWidth = divs[0].offsetWidth;
             var wrapperHeight = divs[1].offsetHeight;
 
-            $scope.wrapperStyle = {};
-            if (wrapperWidth < wrapperComputedWidth) {
-              $scope.wrapperStyle.width = wrapperComputedWidth + 'px';
-            } else {
-              $scope.wrapperStyle.width = wrapperWidth + 'px';
-            }
+            //$scope.wrapperStyle = {};
+            //if (wrapperWidth < wrapperComputedWidth) {
+            //  $scope.wrapperStyle.width = wrapperComputedWidth + 'px';
+            //} else {
+            //  $scope.wrapperStyle.width = wrapperWidth + 'px';
+            //}
 
-            if (wrapperHeight < wrapperComputedHeight && self.size !== 'btn-xs' && self.size !== 'btn-sm') {
-              $scope.wrapperStyle.height = wrapperComputedHeight + 'px';
-            } else {
-              $scope.wrapperStyle.height = wrapperHeight + 'px';
-            }
+            //if (wrapperHeight < wrapperComputedHeight && self.size !== 'btn-xs' && self.size !== 'btn-sm') {
+            //  $scope.wrapperStyle.height = wrapperComputedHeight + 'px';
+            //} else {
+            //  $scope.wrapperStyle.height = wrapperHeight + 'px';
+            //}
 
             $scope.onClass = [self.onstyle, self.size, 'toggle-on'];
             $scope.offClass = [self.offstyle, self.size, 'toggle-off'];
